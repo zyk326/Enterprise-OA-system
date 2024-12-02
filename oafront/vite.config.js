4b3902b16258c7aed58f2c4d9f705c239ea1005f
zyk326 <869550165@qq.com>
2024-12-02 11:31:02 +0800

    change assets path

--- oafront/vite.config.js
+++ oafront/vite.config.js
@@ -7,7 +7,7 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/static/',
+  base: '/assets/',
   build: {
     assetsDir: "oafront"
   },
